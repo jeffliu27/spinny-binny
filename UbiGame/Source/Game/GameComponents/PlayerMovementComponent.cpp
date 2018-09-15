26e29d7bf09932c768c9eace9c2fa2d06bfdabed
Jeff Liu <boyuanliu@live.com>
2018-09-15 13:04:16 -0400

    rotating bird

--- UbiGame/Source/Game/GameComponents/PlayerMovementComponent.cpp
+++ UbiGame/Source/Game/GameComponents/PlayerMovementComponent.cpp
@@ -53,11 +53,15 @@ void PlayerMovementComponent::Update()
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		wantedVel.x -= playerVel * dt;
+		float rotation = GetEntity()->GetRot();
+		GetEntity()->SetRotation(rotation - 0.3);
+		//wantedVel.x -= playerVel * dt;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		wantedVel.x += playerVel * dt;
+		float rotation = GetEntity()->GetRot();
+		GetEntity()->SetRotation(rotation + 0.3);
+		//wantedVel.x += playerVel * dt;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
@@ -76,69 +80,69 @@ void PlayerMovementComponent::Update()
 		}
 	}
 
-	GetEntity()->SetPos(GetEntity()->GetPos() + wantedVel);
+	//GetEntity()->SetPos(GetEntity()->GetPos() + wantedVel);
 
-	if (wantedVel != sf::Vector2f(0.f, 0.f))
-	{
-		m_flyTimerDt = m_flyTimerMaxTime;
-	}
-	else
-	{
-		m_flyTimerDt -= dt;
-	}
+	//if (wantedVel != sf::Vector2f(0.f, 0.f))
+	//{
+	//	m_flyTimerDt = m_flyTimerMaxTime;
+	//}
+	//else
+	//{
+	//	m_flyTimerDt -= dt;
+	//}
 
-	if (m_animComponent)
-	{
-		if (m_flyTimerDt > 0.f)
-		{
-			if (m_animComponent->GetCurrentAnimation() != GameEngine::EAnimationId::BirdFly)
-			{
-				m_animComponent->PlayAnim(GameEngine::EAnimationId::BirdFly);
-			}
-		}
-		else if(m_animComponent->GetCurrentAnimation() != GameEngine::EAnimationId::BirdIdle)
-		{
-			m_animComponent->PlayAnim(GameEngine::EAnimationId::BirdIdle);
-		}
-	}
+	//if (m_animComponent)
+	//{
+	//	if (m_flyTimerDt > 0.f)
+	//	{
+	//		if (m_animComponent->GetCurrentAnimation() != GameEngine::EAnimationId::BirdFly)
+	//		{
+	//			m_animComponent->PlayAnim(GameEngine::EAnimationId::BirdFly);
+	//		}
+	//	}
+	//	else if(m_animComponent->GetCurrentAnimation() != GameEngine::EAnimationId::BirdIdle)
+	//	{
+	//		m_animComponent->PlayAnim(GameEngine::EAnimationId::BirdIdle);
+	//	}
+	//}
 
-	
-	static float rotationVel = 50.f; //Deg/s
-	static float maxRotation = 20.f; //Deg
-	
-	float currentRotation = GetEntity()->GetRot();
-	float wantedRot = 0.f;
-	bool  reseting = false;
+	//
+	//static float rotationVel = 50.f; //Deg/s
+	//static float maxRotation = 20.f; //Deg
+	//
+	//float currentRotation = GetEntity()->GetRot();
+	//float wantedRot = 0.f;
+	//bool  reseting = false;
 
-	if (wantedVel.y > 0.f)
-		wantedRot = rotationVel;
-	else if (wantedVel.y < 0.f)
-		wantedRot = -rotationVel;
-	else
-	{				
-		if (currentRotation > 0.f)
-			wantedRot = -rotationVel;
-		else if (currentRotation < 0.f)
-			wantedRot = rotationVel;
-	}
+	//if (wantedVel.y > 0.f)
+	//	wantedRot = rotationVel;
+	//else if (wantedVel.y < 0.f)
+	//	wantedRot = -rotationVel;
+	//else
+	//{				
+	//	if (currentRotation > 0.f)
+	//		wantedRot = -rotationVel;
+	//	else if (currentRotation < 0.f)
+	//		wantedRot = rotationVel;
+	//}
 
-	float frameRot = wantedRot * dt;
-	float totalRot = currentRotation + frameRot;
+	//float frameRot = wantedRot * dt;
+	//float totalRot = currentRotation + frameRot;
 
-	if (!reseting)
-	{
-		if (totalRot > maxRotation)
-			totalRot = maxRotation;
-		else if (totalRot < -maxRotation)
-			totalRot = -maxRotation;
-	}
-	else
-	{
-		if (currentRotation > 0.f && totalRot < 0.f)
-			totalRot = 0.f;
-		if (currentRotation < 0.f && totalRot > 0.f)
-			totalRot = 0.f;
-	}
+	//if (!reseting)
+	//{
+	//	if (totalRot > maxRotation)
+	//		totalRot = maxRotation;
+	//	else if (totalRot < -maxRotation)
+	//		totalRot = -maxRotation;
+	//}
+	//else
+	//{
+	//	if (currentRotation > 0.f && totalRot < 0.f)
+	//		totalRot = 0.f;
+	//	if (currentRotation < 0.f && totalRot > 0.f)
+	//		totalRot = 0.f;
+	//}
 
-	GetEntity()->SetRotation(totalRot);
+	//GetEntity()->SetRotation(totalRot);
 }
